d0a648a6a02986aef71e8589bd775cf2f97db293
Aurel-Rio <servant.aurelien.developer@gmail.com>
2023-07-21 15:10:17 +0200

    veiltechv0.1

--- back/db.js
+++ back/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise'); // Utiliser mysql2/promise au lieu de mysql2
 
 const pool = mysql.createPool({
   host: 'localhost',
@@ -7,14 +7,16 @@ const pool = mysql.createPool({
   database: 'veiltech',
 });
 
-// Établir la connexion à la base de données
-pool.getConnection((error, connection) => {
-  if (error) {
-    console.error('Erreur de connexion à la base de données :', error);
-  } else {
+// Aucun besoin de pool.getConnection(), car nous utilisons la version promise
+
+// Établir la connexion à la base de données et afficher un message en cas de succès
+pool.getConnection()
+  .then((connection) => {
     console.log('Connexion à la base de données réussie.');
     connection.release(); // Libérer la connexion pour qu'elle puisse être utilisée par d'autres requêtes
-  }
-});
+  })
+  .catch((error) => {
+    console.error('Erreur de connexion à la base de données :', error);
+  });
 
-module.exports = pool.promise(); // Utilisez la version promise du pool pour les requêtes
+module.exports = pool; // Utiliser directement le pool pour les requêtes
